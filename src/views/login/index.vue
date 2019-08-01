f0c43af64d528d21fec884f00c00e4a242c99b6e
你的名字 <>
2019-08-01 20:42:54 +0800

    login init

---
+++ src/views/login/index.vue
@@ -0,0 +1,39 @@
+<template>
+<div class='container'>
+    <el-card class="my-card">
+        <img src="../../assets/image/logo_index.png">
+    </el-card>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="less">
+    .container{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background: url(../../assets/image/login_bg.jpg) no-repeat center / cover
+    }
+    .my-card{
+        width: 400px;
+        height: 350px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%,-50%);
+    img{
+        display: block;
+        width: 200px;
+        margin: 0 auto;
+    }
+    }
+//     .el-card__body{
+//   background: red;
+// }
+
+</style>
