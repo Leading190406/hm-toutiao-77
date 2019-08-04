b080d1b3f7c725c410eb42822c2ee684695a885c
你的名字 <>
2019-08-04 21:07:26 +0800

    使用axios的默认配置

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -22,6 +22,7 @@
 </template>
 
 <script>
+import store from '@/store'
 export default {
   data: function () {
     const checkMobile = (rule, value, callback) => {
@@ -51,6 +52,7 @@ export default {
       this.$refs.loginForm.validate(valid => {
         if (valid) {
           this.$http.post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm).then(res => {
+            store.setUser(res.data.data)
             this.$router.push('/')
           }).catch(() => {
             this.$message.error('手机号或验证码错误')
