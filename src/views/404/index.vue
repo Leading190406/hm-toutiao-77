717baa7e716992a4a8b61eb9817b2d9cb2855658
你的名字 <>
2019-08-05 17:07:07 +0800

    login改成async

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>404</div>
+     <div class='container'></div>
 </template>
 
 <script>
@@ -8,6 +8,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped lang='less'>
+.container{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: url(../../assets/image/404.png) no-repeat center / cover;
+}
 </style>
