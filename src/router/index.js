731eb5097623819470d2d62af643205fe66b25c2
你的名字 <>
2019-08-13 17:20:58 +0800

    fans完成提交

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Image from '@/views/image'
 import Publish from '@/views/publish'
 import Comment from '@/views/comment'
 import Setting from '@/views/setting'
+import Fans from '@/views/fans'
 import store from '@/store'
 Vue.use(VueRouter)
 
@@ -47,6 +48,11 @@ const router = new VueRouter({
           path: '/setting',
           name: 'setting',
           component: Setting
+        },
+        {
+          path: '/fans',
+          name: 'fans',
+          component: Fans
         }
       ]
     },
